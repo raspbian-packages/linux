a7564d324c2882c8182a9337832807f1e41e5b5c
Ben Hutchings <ben@decadent.org.uk>
2012-07-24 03:13:10 +0100

    Include package version along with kernel release in stack traces
    Forwarded: not-needed
    
    For distribution binary packages we assume
    $DISTRIBUTION_OFFICIAL_BUILD, $DISTRIBUTOR and $DISTRIBUTION_VERSION
    are set.
    
    Gbp-Pq: Topic debian
    Gbp-Pq: Name version.patch

--- kernel/hung_task.c
+++ kernel/hung_task.c
@@ -26,6 +26,7 @@
 #include <linux/rwsem.h>
 #include <linux/sys_info.h>
 
+#include <generated/package.h>
 #include <trace/events/sched.h>
 
 /*
@@ -254,10 +255,11 @@ static void check_hung_task(struct task_struct *t, unsigned long timeout,
 			sysctl_hung_task_warnings--;
 		pr_err("INFO: task %s:%d blocked for more than %ld seconds.\n",
 		       t->comm, t->pid, (jiffies - t->last_switch_time) / HZ);
-		pr_err("      %s %s %.*s\n",
+		pr_err("      %s %s %.*s%s\n",
 			print_tainted(), init_utsname()->release,
 			(int)strcspn(init_utsname()->version, " "),
-			init_utsname()->version);
+			init_utsname()->version,
+			LINUX_PACKAGE_ID);
 		if (t->flags & PF_POSTCOREDUMP)
 			pr_err("      Blocked by coredump.\n");
 		pr_err("\"echo 0 > /proc/sys/kernel/hung_task_timeout_secs\""
