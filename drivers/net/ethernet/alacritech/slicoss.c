1bd9a13886911cd0eb72ca541818c0a37b8d25a7
Ben Hutchings <ben@decadent.org.uk>
2012-12-09 16:40:31 +0000

    firmware: Remove redundant log messages from drivers
    Now that firmware_class logs every success and failure consistently,
    many other log messages can be removed from drivers.
    
    This will probably need to be split up into multiple patches prior to
    upstream submission.
    
    
    Gbp-Pq: Topic bugfix/all
    Gbp-Pq: Name firmware-remove-redundant-log-messages-from-drivers.patch

--- drivers/net/ethernet/alacritech/slicoss.c
+++ drivers/net/ethernet/alacritech/slicoss.c
@@ -1052,11 +1052,8 @@ static int slic_load_rcvseq_firmware(struct slic_device *sdev)
 	file = (sdev->model == SLIC_MODEL_OASIS) ?  SLIC_RCV_FIRMWARE_OASIS :
 						    SLIC_RCV_FIRMWARE_MOJAVE;
 	err = request_firmware(&fw, file, &sdev->pdev->dev);
-	if (err) {
-		dev_err(&sdev->pdev->dev,
-			"failed to load receive sequencer firmware %s\n", file);
+	if (err)
 		return err;
-	}
 	/* Do an initial sanity check concerning firmware size now. A further
 	 * check follows below.
 	 */
@@ -1127,10 +1124,8 @@ static int slic_load_firmware(struct slic_device *sdev)
 	file = (sdev->model == SLIC_MODEL_OASIS) ?  SLIC_FIRMWARE_OASIS :
 						    SLIC_FIRMWARE_MOJAVE;
 	err = request_firmware(&fw, file, &sdev->pdev->dev);
-	if (err) {
-		dev_err(&sdev->pdev->dev, "failed to load firmware %s\n", file);
+	if (err)
 		return err;
-	}
 	/* Do an initial sanity check concerning firmware size now. A further
 	 * check follows below.
 	 */
