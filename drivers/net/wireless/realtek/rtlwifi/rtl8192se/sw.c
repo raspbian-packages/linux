cf9673d503a06f0fd56913c41958b9019c2f700e
Ben Hutchings <ben@decadent.org.uk>
2012-12-09 16:40:31 +0000

    firmware: Remove redundant log messages from drivers
    Forwarded: no
    
    Now that firmware_class logs every success and failure consistently,
    many other log messages can be removed from drivers.
    
    This will probably need to be split up into multiple patches prior to
    upstream submission.
    
    
    Gbp-Pq: Topic bugfix/all
    Gbp-Pq: Name firmware-remove-redundant-log-messages-from-drivers.patch

--- drivers/net/wireless/realtek/rtlwifi/rtl8192se/sw.c
+++ drivers/net/wireless/realtek/rtlwifi/rtl8192se/sw.c
@@ -63,12 +63,10 @@ static void rtl92se_fw_cb(const struct firmware *firmware, void *context)
 	struct ieee80211_hw *hw = context;
 	struct rtl_priv *rtlpriv = rtl_priv(hw);
 	struct rt_firmware *pfirmware = NULL;
-	char *fw_name = "rtlwifi/rtl8192sefw.bin";
 
 	rtl_dbg(rtlpriv, COMP_ERR, DBG_LOUD,
 		"Firmware callback routine entered!\n");
 	if (!firmware) {
-		pr_err("Firmware %s not available\n", fw_name);
 		rtlpriv->max_fw_size = 0;
 		goto exit;
 	}
