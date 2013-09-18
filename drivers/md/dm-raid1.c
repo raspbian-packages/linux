6e2db7df565ac71b390433ae52c0e402b7450ece
Ben Hutchings <ben@decadent.org.uk>
2013-09-18 13:22:20 +0000

    dm: Avoid ABI change in 3.2.41
    Commit fd7c092e711e 'dm: fix truncated status strings', backported
    into 3.2.41, changed the return type of target_type::status from void
    to int.  But we can just as easily leave it as int and ignore the
    returned value.
    
    Also, genksyms is too stupid to understand that 'unsigned' is the
    same thing as 'unsigned int'.
    
    
    Gbp-Pq: Topic debian
    Gbp-Pq: Name dm-avoid-ABI-change-in-3.2.41.patch

--- drivers/md/dm-raid1.c
+++ drivers/md/dm-raid1.c
@@ -1358,7 +1358,7 @@ static char device_status_char(struct mirror *m)
 }
 
 
-static void mirror_status(struct dm_target *ti, status_type_t type,
+static int mirror_status(struct dm_target *ti, status_type_t type,
 			  char *result, unsigned maxlen)
 {
 	unsigned int m, sz = 0;
@@ -1394,6 +1394,8 @@ static void mirror_status(struct dm_target *ti, status_type_t type,
 		if (ms->features & DM_RAID1_HANDLE_ERRORS)
 			DMEMIT(" 1 handle_errors");
 	}
+
+	return 0;
 }
 
 static int mirror_iterate_devices(struct dm_target *ti,
