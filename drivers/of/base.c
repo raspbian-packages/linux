ff4a561dbf7f0601148f7cae24203f1c0e33a390
Laurentiu Tudor <b10716@freescale.com>
2014-08-27 17:09:39 +0300

    of: make sure of_alias is initialized before accessing it
    Simply swap of_alias and of_chosen initialization so
    that of_alias ends up read first. This must be done
    because it is accessed couple of lines below when
    trying to initialize the of_stdout using the alias
    based legacy method.
    
    [Fixes a752ee5 - tty: Update hypervisor tty drivers to
    use core stdout parsing code]
    
    Signed-off-by: Laurentiu Tudor <Laurentiu.Tudor@freescale.com>
    [glikely: Don't move the 'if (!of_aliases)' test]
    Signed-off-by: Grant Likely <grant.likely@linaro.org>
    
    Gbp-Pq: Topic features/all
    Gbp-Pq: Name of-make-sure-of_alias-is-initialized-before-accessin.patch

--- drivers/of/base.c
+++ drivers/of/base.c
@@ -2052,6 +2052,7 @@ void of_alias_scan(void * (*dt_alloc)(u64 size, u64 align))
 {
 	struct property *pp;
 
+	of_aliases = of_find_node_by_path("/aliases");
 	of_chosen = of_find_node_by_path("/chosen");
 	if (of_chosen == NULL)
 		of_chosen = of_find_node_by_path("/chosen@0");
@@ -2064,7 +2065,6 @@ void of_alias_scan(void * (*dt_alloc)(u64 size, u64 align))
 			of_stdout = of_find_node_opts_by_path(name, &of_stdout_options);
 	}
 
-	of_aliases = of_find_node_by_path("/aliases");
 	if (!of_aliases)
 		return;
 
