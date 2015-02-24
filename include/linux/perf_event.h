d2f1561e518e11d4c73a0962231f57f1a6f5af90
Ben Hutchings <ben@decadent.org.uk>
2015-02-24 09:53:48 +0000

    perf: Fix ABI change in 3.16.7-ckt6
    Commit c3c87e770458 ('perf: Tighten (and fix) the grouping condition')
    removed perf_event_context::type and its type definition.  Add them
    back.
    
    
    Gbp-Pq: Topic debian
    Gbp-Pq: Name perf-fix-abi-change-in-3.16.7-ckt6.patch

--- include/linux/perf_event.h
+++ include/linux/perf_event.h
@@ -453,6 +453,12 @@ struct perf_event {
 #endif /* CONFIG_PERF_EVENTS */
 };
 
+/* now unused */
+enum perf_event_context_type {
+	task_context,
+	cpu_context,
+};
+
 /**
  * struct perf_event_context - event context structure
  *
@@ -460,6 +466,7 @@ struct perf_event {
  */
 struct perf_event_context {
 	struct pmu			*pmu;
+	enum perf_event_context_type	type; /* now unused */
 	/*
 	 * Protect the states of the events in the list,
 	 * nr_active, and the list:
