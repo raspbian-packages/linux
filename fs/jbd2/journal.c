5e97e1af07fd1e2191e6f02b7410ab96589a420b
Ben Hutchings <ben@decadent.org.uk>
2016-04-13 21:48:06 +0100

    fs: Add MODULE_SOFTDEP declarations for hard-coded crypto drivers
    Bug-Debian: https://bugs.debian.org/819725
    Forwarded: https://lore.kernel.org/all/20160517133631.GF7555@decadent.org.uk/
    
    This helps initramfs builders and other tools to find the full
    dependencies of a module.
    
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>
    [Lukas Wunner: Forward-ported to 4.11: drop parts applied upstream]
    
    Gbp-Pq: Topic bugfix/all
    Gbp-Pq: Name fs-add-module_softdep-declarations-for-hard-coded-cr.patch

--- fs/jbd2/journal.c
+++ fs/jbd2/journal.c
@@ -3194,6 +3194,7 @@ static void __exit journal_exit(void)
 
 MODULE_DESCRIPTION("Generic filesystem journal-writing module");
 MODULE_LICENSE("GPL");
+MODULE_SOFTDEP("pre: crypto-crc32c");
 module_init(journal_init);
 module_exit(journal_exit);
 
