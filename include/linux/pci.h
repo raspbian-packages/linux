bc25e03bf38428919d37303e9f230f47652dae9b
Ben Hutchings <ben@decadent.org.uk>
2016-05-01 15:48:14 +0200

    PCI: Avoid ABI change in 3.2.80
    Move the added bitfield pci_dev::non_compliant_bars to the end of its
    bitfield group and hide it from genksyms.
    
    
    Gbp-Pq: Topic debian
    Gbp-Pq: Name pci-fix-abi-change-in-3.2.80.patch

--- include/linux/pci.h
+++ include/linux/pci.h
@@ -331,7 +331,10 @@ struct pci_dev {
 	unsigned int    is_hotplug_bridge:1;
 	unsigned int    __aer_firmware_first_valid:1;
 	unsigned int	__aer_firmware_first:1;
+#ifndef __GENKSYMS__
 	unsigned int	non_compliant_bars:1;	/* broken BARs; ignore them */
+	/* 9 bits spare */
+#endif
 	pci_dev_flags_t dev_flags;
 	atomic_t	enable_cnt;	/* pci_enable_device has been called */
 
