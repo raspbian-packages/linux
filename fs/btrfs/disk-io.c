6621fa8bfb7a40f460d33fe01d49876b208467fe
Adam Borowski <kilobyte@angband.pl>
2017-03-28 16:55:05 +0200

    btrfs: warn about RAID5/6 being experimental at mount time
    Too many people come complaining about losing their data -- and indeed,
    there's no warning outside a wiki and the mailing list tribal knowledge.
    Message severity chosen for consistency with XFS -- "alert" makes dmesg
    produce nice red background which should get the point across.
    
    Signed-off-by: Adam Borowski <kilobyte@angband.pl>
    [bwh: Also add_taint() so this is flagged in bug reports]
    
    Gbp-Pq: Topic debian
    Gbp-Pq: Name btrfs-warn-about-raid5-6-being-experimental-at-mount.patch

--- fs/btrfs/disk-io.c
+++ fs/btrfs/disk-io.c
@@ -2897,6 +2897,15 @@ retry_root_backup:
 		btrfs_set_and_info(fs_info, SSD, "enabling ssd optimizations");
 	}
 
+	if ((fs_info->avail_data_alloc_bits |
+	     fs_info->avail_metadata_alloc_bits |
+	     fs_info->avail_system_alloc_bits) &
+	    BTRFS_BLOCK_GROUP_RAID56_MASK) {
+		btrfs_alert(fs_info,
+		"btrfs RAID5/6 is EXPERIMENTAL and has known data-loss bugs");
+		add_taint(TAINT_USER, LOCKDEP_STILL_OK);
+	}
+
 	/*
 	 * Mount does not set all options immediately, we can do it now and do
 	 * not have to wait for transaction commit
