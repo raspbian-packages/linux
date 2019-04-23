a83975f591dbd73fae808557bdfc148c05438f11
Robert Holmes <robeholmes@gmail.com>
2019-04-23 07:39:29 +0000

    [PATCH] KEYS: Make use of platform keyring for module signature verify
    Bug-Debian: https://bugs.debian.org/935945
    Origin: https://src.fedoraproject.org/rpms/kernel/raw/master/f/KEYS-Make-use-of-platform-keyring-for-module-signature.patch
    
    This patch completes commit 278311e417be ("kexec, KEYS: Make use of
    platform keyring for signature verify") which, while adding the
    platform keyring for bzImage verification, neglected to also add
    this keyring for module verification.
    
    As such, kernel modules signed with keys from the MokList variable
    were not successfully verified.
    
    Signed-off-by: Robert Holmes <robeholmes@gmail.com>
    Signed-off-by: Jeremy Cline <jcline@redhat.com>
    
    Gbp-Pq: Topic features/all/db-mok-keyring
    Gbp-Pq: Name KEYS-Make-use-of-platform-keyring-for-module-signature.patch

--- kernel/module_signing.c
+++ kernel/module_signing.c
@@ -93,6 +93,13 @@ int mod_verify_sig(const void *mod, struct load_info *info)
 				      VERIFYING_MODULE_SIGNATURE,
 				      NULL, NULL);
 	pr_devel("verify_pkcs7_signature() = %d\n", ret);
+	if (ret == -ENOKEY && IS_ENABLED(CONFIG_INTEGRITY_PLATFORM_KEYRING)) {
+		ret = verify_pkcs7_signature(mod, modlen, mod + modlen, sig_len,
+				VERIFY_USE_PLATFORM_KEYRING,
+				VERIFYING_MODULE_SIGNATURE,
+				NULL, NULL);
+                pr_devel("verify_pkcs7_signature() = %d\n", ret);
+	}
 
 	/* checking hash of module is in blacklist */
 	if (!ret)
