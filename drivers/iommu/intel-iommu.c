1273904d48f283657493d967f0179b89a2d75098
Ben Hutchings <ben@decadent.org.uk>
2019-08-21 00:32:16 +0100

    intel-iommu: Add Kconfig option to exclude iGPU by default
    There is still laptop firmware that touches the integrated GPU behind
    the operating system's back, and doesn't say so in the RMRR table.
    Enabling the IOMMU for all devices causes breakage.
    
    Replace CONFIG_INTEL_IOMMU_DEFAULT_ON with a 3-way choice
    corresponding to "on", "off", and "on,intgpu_off".
    
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>
    
    Gbp-Pq: Topic features/x86
    Gbp-Pq: Name intel-iommu-add-kconfig-option-to-exclude-igpu-by-default.patch

--- drivers/iommu/intel-iommu.c
+++ drivers/iommu/intel-iommu.c
@@ -354,18 +354,14 @@ static int intel_iommu_attach_device(struct iommu_domain *domain,
 static phys_addr_t intel_iommu_iova_to_phys(struct iommu_domain *domain,
 					    dma_addr_t iova);
 
-#ifdef CONFIG_INTEL_IOMMU_DEFAULT_ON
-int dmar_disabled = 0;
-#else
-int dmar_disabled = 1;
-#endif /*CONFIG_INTEL_IOMMU_DEFAULT_ON*/
+int dmar_disabled = IS_ENABLED(CONFIG_INTEL_IOMMU_DEFAULT_OFF);
 
 int intel_iommu_sm;
 int intel_iommu_enabled = 0;
 EXPORT_SYMBOL_GPL(intel_iommu_enabled);
 
 static int dmar_map_gfx = 1;
-static int dmar_map_intgpu = 1;
+static int dmar_map_intgpu = IS_ENABLED(CONFIG_INTEL_IOMMU_DEFAULT_ON);
 static int dmar_forcedac;
 static int intel_iommu_strict;
 static int intel_iommu_superpage = 1;
@@ -446,6 +442,7 @@ static int __init intel_iommu_setup(char *str)
 	while (*str) {
 		if (!strncmp(str, "on", 2)) {
 			dmar_disabled = 0;
+			dmar_map_intgpu = 1;
 			pr_info("IOMMU enabled\n");
 		} else if (!strncmp(str, "off", 3)) {
 			dmar_disabled = 1;
