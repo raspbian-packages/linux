93586569ab27448b9d7150d568f9d834997adfa9
Ben Hutchings <ben@decadent.org.uk>
2019-09-10 11:54:28 +0100

    efi: Lock down the kernel if booted in secure boot mode
    Based on an earlier patch by David Howells, who wrote the following
    description:
    
    > UEFI Secure Boot provides a mechanism for ensuring that the firmware will
    > only load signed bootloaders and kernels.  Certain use cases may also
    > require that all kernel modules also be signed.  Add a configuration option
    > that to lock down the kernel - which includes requiring validly signed
    > modules - if the kernel is secure-booted.
    
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>
    [Salvatore Bonaccorso: After fixing https://bugs.debian.org/956197 the
    help text for LOCK_DOWN_IN_EFI_SECURE_BOOT was adjusted to mention that
    lockdown is triggered in integrity mode (https://bugs.debian.org/1025417)]
    Signed-off-by: Salvatore Bonaccorso <carnil@debian.org>
    
    Gbp-Pq: Topic features/all/lockdown
    Gbp-Pq: Name efi-lock-down-the-kernel-if-booted-in-secure-boot-mo.patch

--- include/linux/security.h
+++ include/linux/security.h
@@ -496,6 +496,7 @@ int security_inode_getsecctx(struct inode *inode, void **ctx, u32 *ctxlen);
 int security_locked_down(enum lockdown_reason what);
 int lsm_fill_user_ctx(struct lsm_ctx __user *uctx, u32 *uctx_len,
 		      void *val, size_t val_len, u64 id, u64 flags);
+int lock_kernel_down(const char *where, enum lockdown_reason level);
 #else /* CONFIG_SECURITY */
 
 static inline int call_blocking_lsm_notifier(enum lsm_event event, void *data)
@@ -1436,6 +1437,11 @@ static inline int security_locked_down(enum lockdown_reason what)
 static inline int lsm_fill_user_ctx(struct lsm_ctx __user *uctx,
 				    u32 *uctx_len, void *val, size_t val_len,
 				    u64 id, u64 flags)
+{
+	return -EOPNOTSUPP;
+}
+static inline int
+lock_kernel_down(const char *where, enum lockdown_reason level)
 {
 	return -EOPNOTSUPP;
 }
