446b3fd463e8b0f15260adb1382b1498d72afc7c
Ben Hutchings <ben@decadent.org.uk>
2019-09-10 11:54:28 +0100

    efi: Lock down the kernel if booted in secure boot mode
    Based on an earlier patch by David Howells, who wrote the following
    description:
    
    > UEFI Secure Boot provides a mechanism for ensuring that the firmware will
    > only load signed bootloaders and kernels.  Certain use cases may also
    > require that all kernel modules also be signed.  Add a configuration option
    > that to lock down the kernel - which includes requiring validly signed
    > modules - if the kernel is secure-booted.
    
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>
    
    Gbp-Pq: Topic features/all/lockdown
    Gbp-Pq: Name efi-lock-down-the-kernel-if-booted-in-secure-boot-mo.patch

--- arch/x86/kernel/setup.c
+++ arch/x86/kernel/setup.c
@@ -1042,6 +1042,8 @@ void __init setup_arch(char **cmdline_p)
 	if (efi_enabled(EFI_BOOT))
 		efi_init();
 
+	efi_set_secure_boot(boot_params.secure_boot);
+
 	dmi_setup();
 
 	/*
@@ -1192,8 +1194,6 @@ void __init setup_arch(char **cmdline_p)
 #endif
 	/* Allocate bigger log buffer */
 	setup_log_buf(1);
-
-	efi_set_secure_boot(boot_params.secure_boot);
 
 	reserve_initrd();
 
