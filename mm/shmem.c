d56595ac3f120560bb6c9268430bbf83d8160644
Ben Hutchings <ben@decadent.org.uk>
2020-09-07 02:51:53 +0100

    Export symbols needed by Android drivers
    Bug-Debian: https://bugs.debian.org/901492
    
    We want to enable use of the Android ashmem and binder drivers to
    support Anbox, but they should not be built-in as that would waste
    resources and increase security attack surface on systems that don't
    need them.
    
    Export the currently un-exported symbols they depend on.
    
    
    Gbp-Pq: Topic debian
    Gbp-Pq: Name export-symbols-needed-by-android-drivers.patch

--- mm/shmem.c
+++ mm/shmem.c
@@ -4932,6 +4932,7 @@ int shmem_zero_setup(struct vm_area_struct *vma)
 
 	return 0;
 }
+EXPORT_SYMBOL_GPL(shmem_zero_setup);
 
 /**
  * shmem_read_folio_gfp - read into page cache, using specified page allocation flags.
