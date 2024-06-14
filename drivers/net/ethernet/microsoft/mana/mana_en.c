4b27b3bca9ddb4a258dbc19406acdc4e23aaee5b
Shradha Gupta <shradhagupta@linux.microsoft.com>
2024-06-14 00:19:08 -0700

    [PATCH 37/44] net: mana: Use mana_cleanup_port_context() for rxq cleanup
    To cleanup rxqs in port context structures, instead of duplicating the
    code, use existing function mana_cleanup_port_context() which does
    the exact cleanup that's needed.
    
    Signed-off-by: Shradha Gupta <shradhagupta@linux.microsoft.com>
    Reviewed-by: Simon Horman <horms@kernel.org>
    Reviewed-by: Wei Liu <wei.liu@kernel.org>
    Reviewed-by: Heng Qi <hengqi@linux.alibaba.com>
    Link: https://lore.kernel.org/r/1718349548-28697-1-git-send-email-shradhagupta@linux.microsoft.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    (cherry picked from commit e275e19c918b502aa4534e56dc2e25b5095e9005)
    Signed-off-by: Bastian Blank <waldi@debian.org>
    
    Gbp-Pq: Topic features/all/ethernet-microsoft
    Gbp-Pq: Name 0037-net-mana-Use-mana_cleanup_port_context-for-rxq-clean.patch

--- drivers/net/ethernet/microsoft/mana/mana_en.c
+++ drivers/net/ethernet/microsoft/mana/mana_en.c
@@ -2370,8 +2370,7 @@ static int mana_init_port(struct net_device *ndev)
 	return 0;
 
 reset_apc:
-	kfree(apc->rxqs);
-	apc->rxqs = NULL;
+	mana_cleanup_port_context(apc);
 	return err;
 }
 
@@ -2619,8 +2618,7 @@ static int mana_probe_port(struct mana_context *ac, int port_idx,
 	return 0;
 
 reset_apc:
-	kfree(apc->rxqs);
-	apc->rxqs = NULL;
+	mana_cleanup_port_context(apc);
 free_net:
 	*ndev_storage = NULL;
 	netdev_err(ndev, "Failed to probe vPort %d: %d\n", port_idx, err);
