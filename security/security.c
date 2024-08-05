41261e6e85d87c318cbe30187cc5f430e21b9a77
Ben Hutchings <ben@decadent.org.uk>
2024-08-05 03:26:48 +0200

    Export symbols needed by binder
    Bug-Debian: https://bugs.debian.org/901492
    
    We want to enable use of the Android binder driver to support
    Waydroid, but it should not be built-in as that would waste resources
    and increase security attack surface on systems that don't need it.
    
    Export the currently un-exported symbols it depends on.
    
    
    Gbp-Pq: Topic debian
    Gbp-Pq: Name export-symbols-needed-by-android-drivers.patch

--- security/security.c
+++ security/security.c
@@ -1029,6 +1029,7 @@ int security_binder_set_context_mgr(const struct cred *mgr)
 {
 	return call_int_hook(binder_set_context_mgr, mgr);
 }
+EXPORT_SYMBOL_GPL(security_binder_set_context_mgr);
 
 /**
  * security_binder_transaction() - Check if a binder transaction is allowed
@@ -1044,6 +1045,7 @@ int security_binder_transaction(const struct cred *from,
 {
 	return call_int_hook(binder_transaction, from, to);
 }
+EXPORT_SYMBOL_GPL(security_binder_transaction);
 
 /**
  * security_binder_transfer_binder() - Check if a binder transfer is allowed
@@ -1059,6 +1061,7 @@ int security_binder_transfer_binder(const struct cred *from,
 {
 	return call_int_hook(binder_transfer_binder, from, to);
 }
+EXPORT_SYMBOL_GPL(security_binder_transfer_binder);
 
 /**
  * security_binder_transfer_file() - Check if a binder file xfer is allowed
@@ -1075,6 +1078,7 @@ int security_binder_transfer_file(const struct cred *from,
 {
 	return call_int_hook(binder_transfer_file, from, to, file);
 }
+EXPORT_SYMBOL_GPL(security_binder_transfer_file);
 
 /**
  * security_ptrace_access_check() - Check if tracing is allowed
